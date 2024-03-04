4939a83d501f4f38dd90ea450a3734bfe50db2fb
Victor <victorbazaldua@geovoy.com>
2024-03-04 10:58:46 -0600

    prueba

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from google.cloud import bigquery
 # Crea un cliente de BigQuery
 client = bigquery.Client()
 query = """
-SELECT DATE(block_timestamp) AS transaction_date, COUNT(*) AS num_transactions
+SELECT DATE(TIMESTAMP_SECONDS(CAST(timestamp / 1000 AS INT64))) AS transaction_date, COUNT(*) AS num_transactions
 FROM `bigquery-public-data.bitcoin_blockchain.transactions`
 GROUP BY transaction_date
 ORDER BY transaction_date;
+;
 """
 
 # Ejecuta la consulta
