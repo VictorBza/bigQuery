02b0992fe19ea7120bdd409121f830ca99d0601b
Victor <victorbazaldua@geovoy.com>
2024-03-06 08:51:51 -0600

    Se suben cosas

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 from google.cloud import bigquery
+from datetime import datetime
 
 # Crea un cliente de BigQuery
 client = bigquery.Client()
 query = """
-SELECT TIMESTAMP_SECONDS(CAST(timestamp / 1000 AS INT64)) AS transaction_timestamp,
-       COUNT(*) AS num_transactions
+SELECT DATE(TIMESTAMP_SECONDS(CAST(timestamp / 1000 AS INT64))) AS transaction_date, COUNT(*) AS num_transactions
 FROM `bigquery-public-data.bitcoin_blockchain.transactions`
-GROUP BY transaction_timestamp
-ORDER BY transaction_timestamp;
+GROUP BY transaction_date
+ORDER BY transaction_date;
 """
 
 # Ejecuta la consulta
 query_job = client.query(query)
 
-# Recorre los resultados
+# Crea un diccionario para almacenar los resultados agrupados por día
+transactions_by_date = {}
+
+# Recorre los resultados y agrupa por día
 for row in query_job:
-    print(row)
+    transaction_date = row['transaction_date']
+    num_transactions = row['num_transactions']
+    transactions_by_date[transaction_date] = num_transactions
 
-
-
-
-    
+# Imprime los resultados agrupados por día
+for date, num_transactions in transactions_by_date.items():
+    print(f"Fecha: {date}, Transacciones: {num_transactions}")
