16f34dfe252fe237074cfb2da63c3ea245ffea77
Victor <victorbazaldua@geovoy.com>
2024-03-06 08:47:28 -0600

    Se suben cosas

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from google.cloud import bigquery
 # Crea un cliente de BigQuery
 client = bigquery.Client()
 query = """
-SELECT DATE(TIMESTAMP_SECONDS(CAST(timestamp / 1000 AS INT64))) AS transaction_date, COUNT(*) AS num_transactions
+SELECT TIMESTAMP_SECONDS(CAST(timestamp / 1000 AS INT64)) AS transaction_timestamp,
+       COUNT(*) AS num_transactions
 FROM `bigquery-public-data.bitcoin_blockchain.transactions`
-GROUP BY transaction_date
-ORDER BY transaction_date;
+GROUP BY transaction_timestamp
+ORDER BY transaction_timestamp;
 """
 
 # Ejecuta la consulta
@@ -15,3 +16,8 @@ query_job = client.query(query)
 # Recorre los resultados
 for row in query_job:
     print(row)
+
+
+
+
+    
